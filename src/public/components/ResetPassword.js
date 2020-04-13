dd67b2e1241110d147b9f62232242611f568f988
Justinas <j.zailskas@eif.viko.lt>
2020-04-13 21:07:35 +0300

    Created Reset Password form

--- src/public/components/ResetPassword.js
+++ src/public/components/ResetPassword.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Grid, Button, Form, Input } from 'semantic-ui-react';
 
 class ResetPassword extends React.Component {
     render() {
         return (
-            <div>
-                <div>
-                    Password reset
-                </div>
-            </div>
+            <Grid centered columns={1}>
+                <Form>
+                    <Form.Field>
+                        <label>Forgot password</label>
+                        <label>You can reset your password here</label>
+                        <Input 
+                            focus
+                            placeholder='Email address'    
+                        />
+                    </Form.Field>
+                    <Button 
+                            primary
+
+                    >Send My Password
+                    </Button>
+                </Form>
+            </Grid>
         )
     }
 }
