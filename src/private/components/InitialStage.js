9d4fc1792697eff01b38a9965019c1b6a65b9f53
Justinas <j.zailskas@eif.viko.lt>
2020-04-30 19:59:20 +0300

    Added maps

--- src/private/components/InitialStage.js
+++ src/private/components/InitialStage.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Grid, GridColumn, Form, Dropdown} from 'semantic-ui-react';
+import ReactMapGL, {Marker} from 'react-map-gl';
 
 const JSON = [
     {key: 1, value: 'Arvydas Sabonis', text: 'Arvydas Sabonis'},
@@ -8,6 +9,38 @@ const JSON = [
     {key: 4, value: 'Saras', text: 'Saras'}
 ]
 class InitialStage extends Component {
+    constructor(props) {
+        super(props);
+          this.state = {
+            viewport: {
+              width: "10vw",
+              height: "10vh",
+              latitude: 42.430472,
+              longitude: -123.334102,
+              zoom: 16
+            },
+            userLocation: { }
+          }
+      }
+      setUserLocation = () => {
+        navigator.geolocation.getCurrentPosition(position => {
+            let setUserLocation = {
+              lat: position.coords.latitude,
+              long: position.coords.longitude
+            };
+            let newViewport = {
+                height: "100vh",
+                width: "100vw",
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+                zoom: 12
+            }
+            this.setState({
+                viewport: newViewport,
+                userLocation: setUserLocation
+            })
+        })
+    }
     render()
         {
             return(
@@ -51,7 +84,21 @@ class InitialStage extends Component {
                             />
                         </Grid.Column>
                         <GridColumn>
-                            <div>Zemelapis</div>
+                            <button onClick={this.setUserLocation}>My location</button>
+                            <ReactMapGL {...this.state.viewport} mapStyle="mapbox://styles/mapbox/outdoors-v11"
+                            onViewportChange={(viewport=> this.setState({viewport}))}
+                            mapboxApiAccessToken="pk.eyJ1IjoianVzdGluYXMiLCJhIjoiY2s5bWNsczExMDM5ZTNlcDlvbHl5ZXY0aSJ9.L4-k6fuXGbaUcoQCKDEqZQ">
+                            {Object.keys(this.state.userLocation).length !==0 ? (
+                                <Marker
+                                latitude={this.state.viewport.latitude}
+                                longitude={this.state.viewport.longitude}
+                                >
+                                    <div>I'm here</div>
+                            </Marker>
+                            ) : (
+                                <div>empty</div>
+                            )}
+                            </ReactMapGL>
                         </GridColumn>
                     </Grid.Row>
 
