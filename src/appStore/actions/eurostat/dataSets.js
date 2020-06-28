924aeb2a59f3cc86d5e1fe04afd9b8b920782de1
Justinas <j.zailskas@eif.viko.lt>
2020-06-29 00:22:04 +0300

    P, R, S, I, J, N  industries added to query file

--- src/appStore/actions/eurostat/dataSets.js
+++ src/appStore/actions/eurostat/dataSets.js
@@ -39,5 +39,152 @@ const dataSet = {
                 variables: [ "V11951", "V11952", "V11953", "V11960", "V16961", "V97451", "V97452", "V97453" ]
             }
         ]
-    }
+    },
+    "P": {
+        dataSets: [
+            {
+                tableCode: "bd_9bd_sz_cl_r2",
+                industries: [ "P", "P85" ],
+                variables: [ "V97015", "V97043", "V97045"]
+            }
+        ]
+    },
+    "R": {
+        dataSets: [
+            {
+                tableCode: "bd_9bd_sz_cl_r2",
+                industries: [ "R", "R90", "R91", "R92", "R93" ],
+                variables: [ "V97015", "V97043", "V97045"]
+            }
+        ]
+    },
+    "S": {
+        dataSets: [
+            {
+                tableCode: "sbs_na_sca_r2",
+                industries: [ "S95" ],
+                variables: [ "V11110", "V12110", "V12120", "V12130", "V12150"]
+            },
+            {
+                tableCode: "sbs_sc_sca_r2",
+                industries: [ "S95" ],
+                variables: [ "V11110"]
+            },
+            {
+                tableCode: "sbs_na_1a_se_r2",
+                industries: [ "S95", "S951", "S9511", "S9512", "S952", "S9521", "S9522", "S9523", "S9524" ],
+                variables: ["V11110", "V12110", "V12120", "V12150", "V12170"]
+            },
+            {
+                tableCode: "sbs_sc_1b_se_r2",
+                industries: [ "S95" ],
+                variables: [ "V11110", "V12110", "V12150", "V16110", "V91100"]
+            },
+            {
+                tableCode: "sbs_r_nuts06_r2",
+                industries: [ "S95" ],
+                variables: [ "V11210"]
+            },
+            {
+                tableCode: "bd_9ac_l_form_r2",
+                industries: [ "S" ],
+                variables: [ "V97010", "V97015", "V97030", "V97130"]
+            },
+            {
+                tableCode: "bd_9ac_l_form_r2",
+                industries: [ "S94", "S95", "S96" ],
+                variables: [ "V97010", "V97015", "V97130"]
+            },
+            {
+                tableCode: "bd_9bd_sz_cl_r2",
+                industries: ["S"],
+                variables: ["V11910", "V11920", "V11930", "V11941", "V11942"]
+            },
+            {
+                tableCode: "bd_9bd_sz_cl_r2",
+                industries: ["S94", "S95", "S96"],
+                variables: ["V11930"]
+            },
+            {
+                tableCode: "bd_9eg_l_form_r2",
+                industries: ["S", "S94", "S95", "S96"],
+                variables: ["V11910", "V11920", "V11930", "V16911", "V16910"]
+            },
+            {
+                tableCode: "bd_9fh_sz_cl_r2",
+                industries: ["S"],
+                variables: ["V11910", "V11920", "V11930", "V11941", "V11942"]
+            },
+            {
+                tableCode: "bd_9fh_sz_cl_r2",
+                industries: ["S94", "S95", "S96"],
+                variables: ["V11910", "V11920", "V11930", "V11942"]
+            },
+            {
+                tableCode: "bd_9pm_r2",
+                industries: ["S95"],
+                variables: ["V11951", "V11952", "V11953", "V11960", "V16961", "V97451", "V97452", "V97453", "V97460", "V97461"]
+            },
+            {
+                tableCode: "bd_9n_r2",
+                industries: ["S", "S94", "S95", "S96"],
+                variables: ["V11950", "V11951", "V11952", "V11953", "V11955"]
+            },
+            {
+                tableCode: "fats_out2_r2",
+                industries: ["S"],
+                variables: ["TUR", "EMP", "ENT"]
+            }, 
+            {
+                tableCode: "fats_out2_r2",
+                industries: ["S", "S94", "S95", "S96"],
+                variables: ["TUR", "EMP"]
+            }, 
+            {
+                tableCode: "fats_out3_r2",
+                industries: ["S", "S94", "S95", "S96"],
+                variables: ["PEC", "GIT", "VAC"]
+            },
+            {
+                tableCode: "bd_9bd_sz_cl_r2",
+                industries: ["S", "S94", "S95", "S96"],
+                variables: ["V97043", "V97045", "V97015"]
+            }
+        ]
+    },
+    "I": {
+        dataSets: [
+            {
+                tableCode: "sbs_na_1a_se_r2",
+                industries: ["I", "I55", "I551", "I5510", "I552", "I5520", "I553", "I5530", "I559", "I5590", "I56", "I561", "I5610", "I562", "I5661", "I5629", "I563", "I5630"],
+                variables: [ "V11110", "V16110", "V13310", "V91100", "V91170", "V92100"]
+            }
+        ]
+    },
+    "J": {
+        dataSets: [
+            {
+                tableCode: "sbs_na_1a_se_r2",
+                industries: ["J", "J58", "J581", "J5811", "J5812", "J5813", "J5814", "J5819", "J582", "J5821", "J5829", "J59", "J591", "J5911", "J5912", "J5913", "J5914", "J592", "J5920", "J60", "J601", "J6010", "J602", "J6020", "J61", "J611", "J6110", "J612", "J6120",  "J613", "J6130", "J619", "J6190", "J62", "J620", "J6201", "J6202", "J6203", "J6209", "J63", "J631", "J6311",  "J6312", "J639", "J6391", "J6399"],
+                variables: [ "V11110", "V12110", "V16110", "V13310", "V91100", "V91170", "V92100"]
+            }
+        ]
+    },
+    "N": {
+        dataSets: [
+            {
+                tableCode: "sbs_na_1a_se_r2",
+                industries: ["N78",  "N781", "N7810", "N782", "N7820", "N783", "N7830", "N79", "N791", "N7911", "N7912", "N799", "N7990", "N80",  "N801", "N8010", "N802", "N8020", "N803",  "N8030", "N81",  "N811", "N8110", "N812", "N8121", "N8122", "N8129", "N813", "N8130", "N82",  "N821", "N8211", "N8219", "N822", "N8220", "N823", "N8230", "N829", "N8291", "N8292", "N8299"],
+                variables: [ "V11110", "V12110", "V16110", "V13310", "V91100", "V91170", "V92100"]
+            },
+            {
+                tableCode: "sbs_na_1a_se_r2",
+                industries: ["N77",  "N771", "N7711", "N7712",  "N772", "N7721", "N7722", "N7729", "N773", "N7731", "N7732", "N7733", "N7734", "N7735", "N7739", "N774", "NN7740", "N81",  "N811", "N8110", "N812", "N8121", "N8122", "N8129", "N813", "N8130"],
+                variables: [ "V11110", "V12110", "V16110"]
+            
+        ]
+    },
+    
+
+    
 }
