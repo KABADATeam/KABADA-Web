b9a4caafe8f0c01b57f1b0812bcd41246ec600d0
Mindaugas Liogys <m.liogys@eif.viko.lt>
2020-06-28 20:15:52 +0300

    DataSets file update

--- src/appStore/actions/eurostat/dataSets.js
+++ src/appStore/actions/eurostat/dataSets.js
@@ -3,10 +3,12 @@ const dataSet = {
         dataSets: [
             {
                 tableCode: "sbs_na_sca_r2",
+                industries: [ "B05", "B06", "B07", "B08", "B09", "B-N_X_K" ],
                 variables: [ "V11110", "V12120", "V12150", "V12170", "V16140", "V91120", "V91170", "V92100", "V92110", "V92111", "V92113" ]
             },
             {
                 tableCode: "bd_9bd_sz_cl_r2",
+                industries: [],
                 variables: [ "V97043", "V97045" ]
             }
         ]
@@ -15,6 +17,7 @@ const dataSet = {
         dataSets: [
             {
                 tableCode: "sbs_na_sca_r2",
+                industries: [ "C10", "C11", "C12", "C13", "C14", "C15", "C16", "C17", "C18", "C19", "C20", "C21", "C22", "C23", "C24", "C25", "C26", "C27", "C28", "C29", "C30", "C31", "C32" ],
                 variables: [ "V11110", "V12120", "V13310", "V13320", "V13330", "V91130", "V91290" ]
             }
         ]
