c928fa1ba3987162a8bc5439e68cdb2ecb30e471
Mindaugas Liogys <m.liogys@eif.viko.lt>
2020-06-28 19:55:04 +0300

    Eurostat query file created

---
+++ src/appStore/actions/eurostat/dataSets.js
@@ -0,0 +1,22 @@
+const dataSet = {
+    "B": {
+        dataSets: [
+            {
+                tableCode: "sbs_na_sca_r2",
+                variables: [ "V11110", "V12120", "V12150", "V12170", "V16140", "V91120", "V91170", "V92100", "V92110", "V92111", "V92113" ]
+            },
+            {
+                tableCode: "bd_9bd_sz_cl_r2",
+                variables: [ "V97043", "V97045" ]
+            }
+        ]
+    },
+    "C": {
+        dataSets: [
+            {
+                tableCode: "sbs_na_sca_r2",
+                variables: [ "V11110", "V12120", "V13310", "V13320", "V13330", "V91130", "V91290" ]
+            }
+        ]
+    }
+}
