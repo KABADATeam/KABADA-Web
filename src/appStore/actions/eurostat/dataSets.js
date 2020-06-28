ff408ec4341aeb5aa98a789f5272c379a40bc47a
xibjrt <jvaiciulyte@gmail.com>
2020-06-28 23:24:52 +0300

    D, G, H, E, L industries added to query file

--- src/appStore/actions/eurostat/dataSets.js
+++ src/appStore/actions/eurostat/dataSets.js
@@ -39,5 +39,95 @@ const dataSet = {
                 variables: [ "V11951", "V11952", "V11953", "V11960", "V16961", "V97451", "V97452", "V97453" ]
             }
         ]
+    },
+    "D": {
+        dataSets: [
+            {
+                tableCode: "sbs_na_sca_r2",
+                industries: [ "D", "D35" ],
+                variables: [ "V11110", "V12120", "V12150", "V12170", "V16140", "V91120", "V91170", "V92100", "V92110", "V92111", "V92113" ]
+            },
+			{
+                tableCode: "bd_9bd_sz_cl_r2",
+                industries: [ "D" ],
+                variables: [ "V97043", "V97045" ]
+            }
+        ]
+    },
+	"G": {
+        dataSets: [
+            {
+                tableCode: "sbs_na_sca_r2",
+                industries: [ "G", "G45", "G46", "G47" ],
+                variables: [ "V11110", "V12110", "V12120", "V12130", "V12170", "V13310", "V16110", "V16130", "V92111" ]
+            },
+			{
+                tableCode: "bd_9bd_sz_cl_r2",
+                industries: [ "G46", "G47", "G471", "G472", "G473", "G474", "G475", "G476", "G477", "G478", "G479" ],
+                variables: [ "V97043", "V97155", "V97163", "V97165", "V97045" ]
+            }
+        ]
+    },
+	"H": {
+        dataSets: [
+            {
+                tableCode: "sbs_na_sca_r2",
+                industries: [ "H49" ],
+                variables: [ "V11110", "V12110", "V12150", "V12170", "V13310", "V15110", "V16130" ]
+            },
+			{
+                tableCode: "sbs_na_sca_r2",
+                industries: [ "H50" ],
+                variables: [ "V11110", "V12110", "V12170", "V13310", "V15110"  ]
+            }, 
+			{
+                tableCode: "sbs_na_sca_r2",
+                industries: [ "H51" ],
+                variables: [ "V11110", "V12110", "V12150", "V12170", "V13310", "V15110", "V16140"  ]
+            }, 
+			{
+                tableCode: "sbs_na_sca_r2",
+                industries: [ "H52" ],
+                variables: [ "V11110", "V12110", "V12150", "V12170", "V13310", "V15110", "V16130"  ]
+            }, 
+			{
+                tableCode: "sbs_na_sca_r2",
+                industries: [ "H53" ],
+                variables: [ "V11110", "V12110", "V12170", "V13120", "V13310", "V15110", "V16110"  ]
+            }, 
+			{
+                tableCode: "bd_9bd_sz_cl_r2",
+                industries: [ "H49", "H50", "H51", "H52", "H53" ],
+                variables: [ "V97043", "V97155", "V97163", "V97165", "V97045"  ]
+            }
+        ]
+    },
+	"E": {
+        dataSets: [
+            {
+                tableCode: "sbs_na_sca_r2",
+                industries: [ "E", "E36", "E37", "E38", "E39" ],
+                variables: [ "V11110", "V12110", "V12120", "V13310", "V16110", "V92111", "V94415"  ]
+            },
+			{
+                tableCode: "bd_9bd_sz_cl_r2",
+                industries: [ "E" ],
+                variables: [ "V97043", "V97045", "V97010", "V97053", "V97055"  ]
+            }
+        ]
+    },
+	"L": {
+        dataSets: [
+            {
+                tableCode: "sbs_na_sca_r2",
+                industries: [ "L", "L68" ],
+                variables: [ "V11110", "V12110", "V12120", "V13310", "V16110", "V92111", "V94415"  ]
+            },
+			{
+                tableCode: "bd_9bd_sz_cl_r2",
+                industries: [ "L", "L68", "L681", "L6810", "L682", "L6820", "L683", "L6831", "L6832" ],
+                variables: [ "V97043", "V97045", "V97010", "V97053", "V97055"  ]
+            }
+        ]
     }
 }
