49693a27a15434ec71e1a43761763a04d89357b3
Mindaugas Liogys <>
2021-04-11 22:46:49 +0300

    Google OAuth enabled

--- src/appStore/actions/authenticationActions.js
+++ src/appStore/actions/authenticationActions.js
@@ -19,6 +19,25 @@ export const login = (email, password) => {
     }
 };
 
+export const googleLogin = (email) => {
+    return async (dispatch, getState) => {
+        dispatch({ type: 'LOADING', payload: true });
+        try
+        {
+            const response = await kabadaAPI.post('api/auth/google', { 'Email': email });
+            dispatch({ type: 'LOGIN_SUCCESS', payload: response.data });       
+        } catch (error) {
+            if (error.response === undefined) {
+                dispatch({ type: 'ERROR', payload: { message: 'Oopsie... System error. Try again, later' } });
+            } else {
+                dispatch({ type: 'ERROR', payload: error.response.data });
+            }            
+        } finally {
+            dispatch({ type: 'LOADING', payload: false });
+        }
+    }
+};
+
 export const register = (name, email, password, callback) => {
     return async (dispatch, getState) => {
         dispatch({ type: 'LOADING', payload: true });
