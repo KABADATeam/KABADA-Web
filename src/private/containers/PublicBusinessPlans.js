938430d83ac55c61f3835aa6533c1cee3782a054
xibjrt <jvaiciulyte@gmail.com>
2021-04-20 10:19:04 +0300

    Changed layout of public business plan page

--- src/private/containers/PublicBusinessPlans.js
+++ src/private/containers/PublicBusinessPlans.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Input, Card, Typography, Space, Table, DatePicker, Select, Row, Col, Avatar, Dropdown, Menu, Form } from 'antd';
+import { Input, Card, Typography, Space, Table, DatePicker, Select, Row, Col, Avatar, Dropdown, Menu, Form, Layout } from 'antd';
 import { SearchOutlined, CaretDownOutlined, CaretDownFilled, UserOutlined } from '@ant-design/icons';
 import { connect } from 'react-redux';
 import { getAllPublicPlans } from "../../appStore/actions/planActions";
 import { iconColor, pageHeaderStyle, filterStyle } from '../../styles/customStyles';
 import '../../css/publicBusinessPlans.css';
 import NewBusinessPlanModal from "../components/NewBusinessPlanModal";
-
+const { Header, Content } = Layout;
 const { Title, Text } = Typography;
 const { Option } = Select;
 
@@ -118,63 +118,67 @@ class PublicBusinessPlans extends React.Component {
             },
         ];
         return (
-            <Row justify="center" align="middle">
-                <Col span={20}>
-                    <Title level={2} style={{ ...pageHeaderStyle, marginTop: '64px', marginBottom: '0px', textAlign: 'left', color: '#262626' }}>Public business plans</Title>
+            <Layout style={{ backgroundColor: '#F5F5F5' }}>
+                <Header></Header>
+                <Content>
+                    <Row wrap={false} justify="center" align="middle">
+                        <Col span={20}>
+                            <Title level={2} style={{ ...pageHeaderStyle, marginBottom: '0px', textAlign: 'left', color: '#262626' }}>Public business plans</Title>
 
-                    <Card style={{ ...publicPlansCardStyle }} bodyStyle={{ width: '100%', paddingTop: '20px', paddingLeft: '0px', paddingRight: '0px', paddingBottom: '0px' }}>
-                        <Form
-                            onValuesChange={this.search.bind(this)}
-                            onFinish={this.onFinish.bind(this)}
-                            style={{ marginBottom: '20px', paddingLeft: '20px', paddingRight: '20px', width: '100%' }}
-                        >
-                            <Row gutter={[16, 20]}>
-                                <Col span={8}>
-                                    <Form.Item noStyle={true} name="name">
-                                        <Input style={filterStyle} placeholder="Search by name..." prefix={<SearchOutlined style={iconColor} />} />
-                                    </Form.Item>
-                                </Col>
-                                <Col span={8}>
-                                    <Form.Item noStyle={true} name="industry">
-                                        <Select dropdownClassName="filterSelect" placeholder="Choose Industry" suffixIcon={<CaretDownOutlined style={iconColor} />} allowClear>
-                                            <Option value="Industry">Industry</Option>
-                                        </Select>
-                                    </Form.Item>
-                                </Col>
-                                <Col span={8}>
-                                    <Form.Item noStyle={true} name="sorting">
-                                        <Select dropdownClassName="filterSelect" placeholder="Sort by: Newest" suffixIcon={<CaretDownOutlined style={iconColor} />} allowClear>
-                                            <Option value="newest">Sort by: Newest</Option>
-                                            <Option value="oldest">Sort by: Oldest</Option>
-                                        </Select>
-                                    </Form.Item>
-                                </Col>
-                                <Col span={8}>
-                                    <Form.Item noStyle={true} name="date">
-                                        <DatePicker style={filterStyle} placeholder="Date Created" />
-                                    </Form.Item>
-                                </Col>
-                                <Col span={8}>
-                                    <Form.Item noStyle={true} name="country">
-                                        <Select dropdownClassName="filterSelect" placeholder="Choose Country" suffixIcon={<CaretDownOutlined style={iconColor} />} allowClear>
-                                            <Option value="Country">Country</Option>
-                                        </Select>
-                                    </Form.Item>
-                                </Col>
-                                <Col span={8}>
-                                    <Form.Item noStyle={true} name="language">
-                                        <Select dropdownClassName="filterSelect" placeholder="Choose Language" suffixIcon={<CaretDownOutlined style={iconColor} />} allowClear>
-                                            <Option value="Language">Language</Option>
-                                        </Select>
-                                    </Form.Item>
-                                </Col>
-                            </Row>
-                        </Form>
-                        <Table style={{ width: '100%' }} size="default" columns={columns} pagination={{ defaultPageSize: 5, showTotal: (total, range) => <Text style={{ position: 'absolute', left: '50%', transform: 'translate(-50%)' }}>{range[0]}-{range[1]} of {total}</Text>, position: ['bottomLeft'] }} dataSource={data} onChange={this.handleChange} />
-                    </Card >
-                </Col>
-            </Row >
-
+                            <Card style={{ ...publicPlansCardStyle }} bodyStyle={{ width: '100%', paddingTop: '20px', paddingLeft: '0px', paddingRight: '0px', paddingBottom: '0px' }}>
+                                <Form
+                                    onValuesChange={this.search.bind(this)}
+                                    onFinish={this.onFinish.bind(this)}
+                                    style={{ marginBottom: '20px', paddingLeft: '20px', paddingRight: '20px', width: '100%' }}
+                                >
+                                    <Row gutter={[16, 20]}>
+                                        <Col span={8}>
+                                            <Form.Item noStyle={true} name="name">
+                                                <Input style={filterStyle} placeholder="Search by name..." prefix={<SearchOutlined style={iconColor} />} />
+                                            </Form.Item>
+                                        </Col>
+                                        <Col span={8}>
+                                            <Form.Item noStyle={true} name="industry">
+                                                <Select dropdownClassName="filterSelect" placeholder="Choose Industry" suffixIcon={<CaretDownOutlined style={iconColor} />} allowClear>
+                                                    <Option value="Industry">Industry</Option>
+                                                </Select>
+                                            </Form.Item>
+                                        </Col>
+                                        <Col span={8}>
+                                            <Form.Item noStyle={true} name="sorting">
+                                                <Select dropdownClassName="filterSelect" placeholder="Sort by: Newest" suffixIcon={<CaretDownOutlined style={iconColor} />} allowClear>
+                                                    <Option value="newest">Sort by: Newest</Option>
+                                                    <Option value="oldest">Sort by: Oldest</Option>
+                                                </Select>
+                                            </Form.Item>
+                                        </Col>
+                                        <Col span={8}>
+                                            <Form.Item noStyle={true} name="date">
+                                                <DatePicker style={filterStyle} placeholder="Date Created" />
+                                            </Form.Item>
+                                        </Col>
+                                        <Col span={8}>
+                                            <Form.Item noStyle={true} name="country">
+                                                <Select dropdownClassName="filterSelect" placeholder="Choose Country" suffixIcon={<CaretDownOutlined style={iconColor} />} allowClear>
+                                                    <Option value="Country">Country</Option>
+                                                </Select>
+                                            </Form.Item>
+                                        </Col>
+                                        <Col span={8}>
+                                            <Form.Item noStyle={true} name="language">
+                                                <Select dropdownClassName="filterSelect" placeholder="Choose Language" suffixIcon={<CaretDownOutlined style={iconColor} />} allowClear>
+                                                    <Option value="Language">Language</Option>
+                                                </Select>
+                                            </Form.Item>
+                                        </Col>
+                                    </Row>
+                                </Form>
+                                <Table style={{ width: '100%' }} size="default" columns={columns} pagination={{ defaultPageSize: 5, showTotal: (total, range) => <Text style={{ position: 'absolute', left: '50%', transform: 'translate(-50%)' }}>{range[0]}-{range[1]} of {total}</Text>, position: ['bottomLeft'] }} dataSource={data} onChange={this.handleChange} />
+                            </Card >
+                        </Col>
+                    </Row >
+                </Content>
+            </Layout>
         );
     }
 }
