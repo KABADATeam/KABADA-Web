c1d040bf2c158f044a8dcfb73c24632158e866ef
xibjrt <jvaiciulyte@gmail.com>
2021-04-20 13:19:54 +0300

    Added modal window to personal business plan

--- src/private/components/NewBusinessPlanModal.js
+++ src/private/components/NewBusinessPlanModal.js
@@ -8,15 +8,8 @@ class NewBusinessPlanModal extends Component {
 
     state = {
         fileList: [],
-        visibility: false,
+        isVisible: false,
     };
-
-
-    componentDidMount() {
-        this.setState({
-            visibility: this.props.visibility,
-        });
-    }
 
     handleOk = (values) => {
         console.log('Clicked ok button');
@@ -33,14 +26,15 @@ class NewBusinessPlanModal extends Component {
         formData.append('name', values.name);
 
         this.setState({
-            visibility: true,
+            isVisible: true,
         });
     };
 
     handleCancel = () => {
+        this.props.handleClose();
         console.log('Clicked cancel button');
         this.setState({
-            visibility: false,
+            isVisible: false,
         });
     };
 
@@ -56,7 +50,7 @@ class NewBusinessPlanModal extends Component {
 
     render() {
 
-        const visibility = this.state.visibility;
+        const isVisible = this.props.visibility;
         const { fileList } = this.state;
 
         const propsUpload = {
@@ -87,45 +81,47 @@ class NewBusinessPlanModal extends Component {
         };
 
         return (
-            <Modal
-                bodyStyle={{ paddingBottom: '0px' }}
-                centered={true}
-                title="New business plan"
-                visible={visibility}
-                okText="Create"
-                footer={[
-                    <>
-                        <Button key="cancel" onClick={this.handleCancel.bind(this)}>Cancel</Button>
-                        <Button key="submit" form="myForm" key="submit" htmlType="submit" type={'primary'}>Submit</Button>
-                    </>
-                ]}
-            >
-                <Form layout="vertical" id="myForm" name="myForm" onFinish={this.handleOk}>
-                    <Form.Item key="name" name="name" label="Project name"
-                        rules={[
-                            {
-                                validator: async (_, name) => {
-                                    if (!name || name.length < 1) {
-                                        return Promise.reject(new Error('Enter project name'));
-                                    }
+            <>
+                <Modal
+                    bodyStyle={{ paddingBottom: '0px' }}
+                    centered={true}
+                    title="New business plan"
+                    visible={isVisible}
+                    onCancel={this.handleCancel}
+                    footer={
+                        <div>
+                            <Button key="customCancel" onClick={this.handleCancel.bind(this)}>Cancel</Button>
+                            <Button key="customSubmit" form="myForm" key="submit" htmlType="submit" type={'primary'}>Submit</Button>
+                        </div>
+                    }
+                >
+                    <Form layout="vertical" id="myForm" name="myForm" onFinish={this.handleOk}>
+                        <Form.Item key="name" name="name" label="Project name"
+                            rules={[
+                                {
+                                    validator: async (_, name) => {
+                                        if (!name || name.length < 1) {
+                                            return Promise.reject(new Error('Enter project name'));
+                                        }
+                                    },
                                 },
-                            },
-                        ]}>
-                        <Input size="large" style={inputStyle} />
-                    </Form.Item>
-                    <Form.Item
-                        key="upload"
-                        name="upload"
-                        label="Cover image (optional)"
-                        valuePropName="fileList"
-                        getValueFromEvent={this.normFile}
-                    >
-                        <Upload key="files" {...propsUpload} maxCount={1} name="files" accept="image/*">
-                            <Button style={buttonStyle} icon={<UploadOutlined />}>Browse</Button>
-                        </Upload>
-                    </Form.Item>
-                </Form>
-            </Modal >
+                            ]}>
+                            <Input size="large" style={inputStyle} />
+                        </Form.Item>
+                        <Form.Item
+                            key="upload"
+                            name="upload"
+                            label="Cover image (optional)"
+                            valuePropName="fileList"
+                            getValueFromEvent={this.normFile}
+                        >
+                            <Upload key="files" {...propsUpload} maxCount={1} name="files" accept="image/*">
+                                <Button style={buttonStyle} icon={<UploadOutlined />}>Browse</Button>
+                            </Upload>
+                        </Form.Item>
+                    </Form>
+                </Modal >
+            </>
         )
     }
 }
--- src/private/containers/PersonalBusinessPlans.js
+++ src/private/containers/PersonalBusinessPlans.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Row, Col, Typography, Button, Tabs, Card } from 'antd';
 import { buttonStyle } from '../../styles/customStyles';
 import PlanElementComponent from '../components/PlanElementComponent';
+import NewBusinessPlanModal from "../components/NewBusinessPlanModal";
 
 const { Title } = Typography;
 const { TabPane } = Tabs;
@@ -10,7 +11,28 @@ const position = { 'position': 'fixed', 'left': '50%', 'transform': 'translate(-
 
 class PersonalBusinessPlans extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            isVisible: false,
+        };
+    }
+
+    openModal = () => {
+        this.setState({
+            isVisible: true,
+        })
+    };
+
+    closeModal = () => {
+        console.log('Clicked cancel button');
+        this.setState({
+            isVisible: false,
+        });
+    };
+
     render() {
+        const isVisible = this.state.isVisible;
         return (
             <Row style={{ ...position, marginTop: 64, width: 1200 }}>
                 <Col span={12} style={{ marginTop: 41 }}>
@@ -29,25 +51,27 @@ class PersonalBusinessPlans extends Component {
                             type="primary"
                             style={{ ...buttonStyle, marginLeft: 32 }}
                             size="large"
+                            onClick={this.openModal.bind(this)}
                         >
                             Create Plan
                         </Button>
                     </div>
                 </Col>
                 <Tabs defaultActiveKey="1" >
-                        <TabPane tab="All" key="1">
-                            <PlanElementComponent/>
+                    <TabPane tab="All" key="1">
+                        <PlanElementComponent />
+                    </TabPane>
+                    <TabPane tab="In Progress" key="2">
+                        COntent of Tab 2
                         </TabPane>
-                        <TabPane tab="In Progress" key="2">
-                            COntent of Tab 2
+                    <TabPane tab="Completed" key="3">
+                        Content of Tab 3
                         </TabPane>
-                        <TabPane tab="Completed" key="3">
-                            Content of Tab 3
-                        </TabPane>
-                        <TabPane tab="Shared With me" key="4">
-                            Content of Tab 4
+                    <TabPane tab="Shared With me" key="4">
+                        Content of Tab 4
                         </TabPane>
                 </Tabs>
+                <NewBusinessPlanModal visibility={isVisible} handleClose={this.closeModal} />
             </Row>
         );
     }
