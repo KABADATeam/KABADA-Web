e68dc3552876d12caea8d8c97e5ed713e239df69
Mindaugas Liogys <>
2021-06-02 18:32:20 +0300

    Footer position fix

--- src/styles/customStyles.js
+++ src/styles/customStyles.js
@@ -60,8 +60,7 @@ export const footerStyle = {
     backgroundColor: '#F5F5F5',
     position: 'absolute',
     bottom: '0px',
-    width: '100%',
-    height: '20px'
+    width: '100%'
 }
 
 export const iconColor = {
