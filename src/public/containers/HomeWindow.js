57f2906c3dbb51c8343a7bda479cbe8fec94500e
Mindaugas Liogys <>
2021-06-26 18:09:05 +0300

    HomePage design update

--- src/public/containers/HomeWindow.js
+++ src/public/containers/HomeWindow.js
@@ -6,7 +6,7 @@ class HomeWindow extends React.Component {
     render() {        
         return (
             <>
-                <SiteHeader />
+                <SiteHeader />             
             </>          
         );
     }
--- src/public/containers/SiteHeader.js
+++ src/public/containers/SiteHeader.js
@@ -1,36 +1,32 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Row, Col, Menu } from 'antd';
+import KabadaIcon from '../../private/components/KabadaIcon';
+
+const headerStyles = {
+    width: '100%', 
+    height: 64, 
+    backgroundColor: '#FFFFFF',
+}
 
 class SiteHeader extends React.Component {
     render() {
         return (
-            <>
-                <Row align='middle' style={{ backgroundColor: 'lightgray', height: '70px' }}>
-                    <Col span={4}>
-                        <img src='kabada_logo.png' style={{ height: '60px'}} alt='Kabada logo :)' />
-                    </Col>
-                    <Col span={20}>
-                        <Menu mode="horizontal" style={{ height: '65px', float: 'right', backgroundColor: 'inherit', borderBottom: 'none' }}>
-                            <Menu.Item key='about'>
-                                <Link to='/about'>
-                                    <h4 style={{ marginTop: '10px' }}>About</h4>
-                                </Link>
-                            </Menu.Item>
-                            <Menu.Item key='privacy'>
-                                <Link to='/privacy'>
-                                    <h4>Privacy Policy</h4>
-                                </Link>
-                            </Menu.Item>
-                            <Menu.Item name='login'>
-                                <Link to='/login'>
-                                    <h4>Login</h4>
-                                </Link>
-                            </Menu.Item>
-                        </Menu>
-                    </Col>
-                </Row>
-            </>
+            <Row style={headerStyles} align="middle">
+                <Col span={4}>
+                    <div style={{paddingLeft:24, display: 'flex', alignItems: 'center'}}>
+                        <KabadaIcon/>
+                    </div>
+                    
+                </Col>
+                <Col span={20} >
+                    <div style={{float: 'right', marginRight: 24}}>
+                        <Link to='/login'>
+                            <h4>Login</h4>
+                        </Link>
+                    </div>
+                </Col>
+            </Row>    
         )
     }
 }
