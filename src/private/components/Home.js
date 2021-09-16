5ed20728b59db2df4e8ecf4179d33832bfda04bf
songulija <65647664+songulija@users.noreply.github.com>
2021-09-16 08:37:08 +0300

    Pridetas taskas

--- src/private/components/Home.js
+++ src/private/components/Home.js
@@ -4,7 +4,7 @@ class Home extends React.Component {
     render() {
         return (
             <div>
-                This is initial user account page
+                This is initial user account page.
             </div>
         )
     }
