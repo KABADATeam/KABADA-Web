db70994ae15be223b436f56003791b20cfaa446f
PC <lsongulija@gmail.com>
2021-09-26 18:40:52 +0200

    Added validation for 'Select up to 5 sources' in PriceLevelComponent

--- src/private/components/new_product/PriceLevelComponent.js
+++ src/private/components/new_product/PriceLevelComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Typography, Space, Card, Divider, Select, Checkbox } from 'antd';
+import { Typography, Space, Card, Divider, Select, Checkbox,Col,Row } from 'antd';
 import { cardStyle, tableCardBodyStyle } from '../../../styles/customStyles';
 import { setProductPriceLevel, setIncomeSources } from "../../../appStore/actions/productActions";
 
@@ -20,6 +20,26 @@ const descriptionTextStyle = {
     color: '#8C8C8C',
 }
 class PriceLevelComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          checked: []
+        };
+      }
+      //everytime you check checkbox it will add id of income source to checked array ['7878787','898954654654654']
+      onChange = checkedValues => {
+          console.log(checkedValues)
+        this.setState(() => {
+          return { checked: checkedValues };
+        });
+        this.props.setIncomeSources(checkedValues);
+      };
+    
+      isDisabled = id => {
+        return (
+          this.state.checked.length > 4 && this.state.checked.indexOf(id) === -1
+        );
+      };
 
     onSelectionChange(id) {
         this.props.setProductPriceLevel(id);
@@ -36,6 +56,7 @@ class PriceLevelComponent extends Component {
         const checkBoxes = this.props.incomeSources.map((obj) =>
             <Checkbox value={obj.id} key={obj.key}>{obj.title}</Checkbox>
         );
+        
 
         return (
             <>
@@ -49,11 +70,48 @@ class PriceLevelComponent extends Component {
                     <Space direction="vertical">
                         <Text style={infoTextStyle}>Additional income sources</Text>
                         <Text style={descriptionTextStyle}>Select up to 5 sources</Text>
-                        <Checkbox.Group onChange={this.onIncomeSourcesChanged}>
+                        {/* <Checkbox.Group onChange={this.onIncomeSourcesChanged}>
                             <Space direction="vertical">
                                 {checkBoxes}
                             </Space>
+                        </Checkbox.Group> */}
+                        <Checkbox.Group onChange={this.onChange}>
+                            <Space direction="vertical">
+                            {this.props.incomeSources.map((obj) => (
+                               <Checkbox value={obj.id} key={obj.key} disabled={this.isDisabled(obj.id)}>{obj.title}</Checkbox>
+                            ))}
+                            </Space>
                         </Checkbox.Group>
+
+                        {/* <Checkbox.Group style={{ width: "100%" }} onChange={this.onChange}>
+        <Row>
+          <Col span={8}>
+            <Checkbox value="A" disabled={this.isDisabled("A")}>
+              A
+            </Checkbox>
+          </Col>
+          <Col span={8}>
+            <Checkbox value="B" disabled={this.isDisabled("B")}>
+              B
+            </Checkbox>
+          </Col>
+          <Col span={8}>
+            <Checkbox value="C" disabled={this.isDisabled("C")}>
+              C
+            </Checkbox>
+          </Col>
+          <Col span={8}>
+            <Checkbox value="D" disabled={this.isDisabled("D")}>
+              D
+            </Checkbox>
+          </Col>
+          <Col span={8}>
+            <Checkbox value="E" disabled={this.isDisabled("E")}>
+              E
+            </Checkbox>
+          </Col>
+        </Row>
+      </Checkbox.Group> */}
                     </Space>
                 </Card>
             </>
