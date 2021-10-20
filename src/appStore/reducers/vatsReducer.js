b4ecfaeb6c4c4c1718bc927299f2af3ef0ae766a
Justinas Zailskas <j.zailskas@eif.viko.lt>
2021-10-20 22:57:09 +0300

    Fixed VAT in financial projections

--- src/appStore/reducers/vatsReducer.js
+++ src/appStore/reducers/vatsReducer.js
@@ -1,23 +1,9 @@
 export const vatsReducer = (
-    state = {
-        vat: []
-    }, action) => {
+    state = {}, action) => {
     switch (action.type) {
         case 'FETCHING_VAT_SUCCESS':
-            let vatArray = [];
-            let objectValues = Object.values(action.payload);
-            let objectPropertyName = Object.keys(action.payload);
-            let length = objectPropertyName.length
-            for (let i = 0; i < length; i++) {
-                if (objectValues[i] !== null) {
-                    vatArray.push({
-                        key: i,
-                        vatTitle: objectPropertyName[i],
-                        vatValue: objectValues[i]
-                    })
-                }
-            }
-            return { ...state, vat: vatArray };
+            const country_vats = action.payload;
+            return { ...action.payload};
         default:
             return state;
     }
