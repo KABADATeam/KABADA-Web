f12e1c4259a47fcb3aaaebce55684027ebbd818f
Justinas Zailskas <j.zailskas@eif.viko.lt>
2022-01-18 13:55:58 +0200

    Edit Sales forecast design, add tooltips

--- src/private/components/sales_Forecast/SalesForecastTable.js
+++ src/private/components/sales_Forecast/SalesForecastTable.js
@@ -1,17 +1,44 @@
 import React from 'react'
-import { Table } from 'antd';
+import { Table, Typography } from 'antd';
+import TooltipComponent from '../Tooltip';
+
+const { Text } = Typography;
 
 function SalesForecastTable({ columns, dataSource, title }) {
-
-    return (
-        <div>
-
-
-            <Table title={() => title} columns={columns} dataSource={dataSource} pagination={false} />
-
-
-        </div>
-    )
+    console.log(title);
+    if (title === 'Sales forecast in EU') {
+        return (
+            <div>
+                <Table
+                    title={() =>
+                        <>
+                            <Text>Sales forecast in EU</Text>
+                            <TooltipComponent code='salesforecast4' type='text' />
+                        </>
+                    }
+                    columns={columns}
+                    dataSource={dataSource}
+                    pagination={false}
+                />
+            </div>
+        )
+    } else if (title === 'Sales forecast outside EU') {
+        return (
+            <div>
+                <Table
+                    title={() =>
+                        <>
+                            <Text>Sales forecast outside EU</Text>
+                            <TooltipComponent code='salesforecast5' type='text' />
+                        </>
+                    }
+                    columns={columns}
+                    dataSource={dataSource}
+                    pagination={false}
+                />
+            </div>
+        )
+    }
 }
 
 export default SalesForecastTable;
