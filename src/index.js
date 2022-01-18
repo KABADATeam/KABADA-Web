3647b67ee3098ee005ce98c57d12ab8a06602769
songulija <65647664+songulija@users.noreply.github.com>
2022-01-18 17:02:25 +0200

    Added save user in  Cookies when Login

--- src/index.js
+++ src/index.js
@@ -7,8 +7,18 @@ import thunk from 'redux-thunk';
 import reducers from './appStore/reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import 'antd/dist/antd.css';
+import Cookies from 'js-cookie'
 
-export const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+
+const user_access_token_from_storage = Cookies.get('access_token') ? Cookies.get('access_token') : null;
+// localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null;
+const user_email_from_storage = Cookies.get('email') ? Cookies.get('email') : null;
+const user_name_from_storage = Cookies.get('name') ? Cookies.get('name') : null;
+
+const initialState = {
+    user: {access_token: user_access_token_from_storage,email: user_email_from_storage,name: user_name_from_storage}
+}
+export const store = createStore(reducers,initialState, composeWithDevTools(applyMiddleware(thunk)));
 
 ReactDOM.render(
 	<Provider store={store}>
