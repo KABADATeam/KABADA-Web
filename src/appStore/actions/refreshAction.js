941e13eaa965c98d0ebc8a70b1a77716174dffdc
Justinas Zailskas <j.zailskas@eif.viko.lt>
2022-02-01 16:54:21 +0200

    Fixed Assets refresh problem

--- src/appStore/actions/refreshAction.js
+++ src/appStore/actions/refreshAction.js
@@ -6,17 +6,20 @@ export const refreshPlan = (planId, callback) => {
             const token = getState().user.access_token;
             const [response] = await Promise.all([kabadaAPI.get("api/plans", { headers: { Authorization: `Bearer ${token}` } })]);
             dispatch({ type: "FETCHING_PLANS_SUCCESS", payload: response.data.privateBusinessPlans.businessPlan });
-            const response_tooltip = await kabadaAPI.get('api/Technical/tooltips', { headers: { Authorization: `Bearer ${token}` } });
-            dispatch({ type: "FETCHING_TOOLTIPS_SUCCESS", payload: response_tooltip.data});
-            const plan = getState().personalBusinessPlans.find(x => x.id === planId);
+            // const response_tooltip = await kabadaAPI.get('api/Technical/tooltips', { headers: { Authorization: `Bearer ${token}` } });
+            // dispatch({ type: "FETCHING_TOOLTIPS_SUCCESS", payload: response_tooltip.data});
             const response_nace = await kabadaAPI.get('/api/nace/wood');
             dispatch({ type: 'FETCHING_NACE_SUCCESS', payload: response_nace.data });
             const response_countries = await kabadaAPI.get('api/countries' );
             dispatch({ type: 'COUNTRIES_FETCH_SUCCESS', payload: response_countries.data });
             const response_languages = await kabadaAPI.get('api/languages');
             dispatch({ type: 'PLAN_LANGUAGES_FETCH_SUCCESS', payload: response_languages.data }); 
-            console.log(plan)
+            const plan = getState().personalBusinessPlans.find(x => x.id === planId);
+            console.log(plan);
             dispatch({ type: 'FETCHING_SELECTED_PLAN_SUCCESS', payload: plan });
+            const response_plan_details = await kabadaAPI.post("api/plans/fetch", { "Id": planId }, { headers: { Authorization: `Bearer ${token}` } });
+            console.log(response_plan_details.data);
+            dispatch({ type: "FETCHING_SELECTED_PLAN_DETAILS_SUCCESS", payload: response_plan_details.data });
             const response2 = await kabadaAPI.get("api/files/" + plan.planImage, { headers: { Authorization: `Bearer ${token}` }, responseType: 'arraybuffer' });
             dispatch({ type: "UPDATING_SELECTED_PLAN_SUCCESS", payload: { ...plan, coverImage: getURL(response2.data) } });
         } finally {
