3c267b81dc2f4a5585e4bd8ba35c3f979ccf115d
Justinas Zailskas <j.zailskas@eif.viko.lt>
2022-02-18 13:13:03 +0200

    Issue 10 solved

--- src/public/containers/AuthenticationWindow.js
+++ src/public/containers/AuthenticationWindow.js
@@ -1,25 +1,36 @@
 import React, { Component } from 'react';
 import FooterComponent from '../components/FooterComponent';
-import {Layout} from 'antd';
+import { Layout, Row, Col } from 'antd';
 
-const { Content } = Layout;
+const { Content, Footer, Header } = Layout;
 
 class AuthenticationWindow extends Component {
 
-    render() {        
+    render() {
         return (
-            <>
-                <div style={{ minHeight: 'calc(100% - 50px)', width: '100%', backgroundColor: 'rgb(245, 245, 245)' }}>
-                    <Layout style={{ position: "absolute", top: "50%", transform: "translateY(-50%)", width: "100%", backgroundColor: 'rgb(245, 245, 245)' }}>
-                        <Content style={{ width: "486px", marginLeft: "auto", marginRight: "auto" }}>
-                            {this.props.children}
-                        </Content>
-                    </Layout>
-                </div>
-                <div>
-                    <FooterComponent alignment='center' divider={false}/>
-                </div>
-            </>
+            <Layout className='layout' style={{width: "100%", minHeight: '100vh', backgroundColor: 'rgb(245, 245, 245)', }}>
+                <Header style={{ backgroundColor: 'rgb(245, 245, 245)' }} />
+                <Content style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+                    <div style={{width: '486px'}}>
+                        {this.props.children}
+                    </div>
+                </Content>
+                <Footer style={{ backgroundColor: 'rgb(245, 245, 245)' }}>
+                    <FooterComponent alignment='center' divider={false} />
+                </Footer>
+            </Layout>
+            // <>
+            //     <div style={{ minHeight: 'calc(100% - 50px)', width: '100%', backgroundColor: 'rgb(245, 245, 245)' }}>
+            //         <Layout style={{  backgroundColor: 'rgb(245, 245, 245)' }}>
+            //             <Content style={{ width: "486px", marginLeft: "auto", marginRight: "auto" }}>
+            //                 {this.props.children}
+            //             </Content>
+            //         </Layout>
+            //     </div>
+            //     <div>
+            //         <FooterComponent alignment='center' divider={false}/>
+            //     </div>
+            // </>
         );
     }
 }
