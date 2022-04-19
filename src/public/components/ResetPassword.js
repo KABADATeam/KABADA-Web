d98cecc30eb84ecf8769d51fd8d08ce6a047a5e3
songulija <lukas.songulija@acosta.com>
2022-04-19 21:48:24 +0300

    Error 167. Fixed not required error

--- src/public/components/ResetPassword.js
+++ src/public/components/ResetPassword.js
@@ -6,72 +6,87 @@ import { forgotPassword } from '../../appStore/actions/authenticationActions';
 import { connect } from 'react-redux';
 import KabadaIcon from './KabadaIcon';
 
-import { cardStyle, buttonStyle, textColor, linkStyle } from '../../styles/customStyles';
+import {
+  cardStyle,
+  buttonStyle,
+  textColor,
+  linkStyle,
+} from '../../styles/customStyles';
 
-const spaceAlignContainer = { 'display': 'flex', 'flexWrap': 'wrap', 'justifyContent': 'center' };
+const spaceAlignContainer = {
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'center',
+};
 
 const { Title } = Typography;
 
 class ResetPassword extends Component {
+  onFinish = (e) => {
+    this.props.forgotPassword(e.email, () => {
+      this.props.history.push('/password-sent');
+    });
+  };
 
-    onFinish = (values) => {
-        this.props.forgotPassword(values.email, () => {
-            this.props.history.push("/password-sent");
-        });
-    };
+  render() {
+    return (
+      <Card style={{ ...cardStyle, ...textColor }} bodyStyle={{ padding: '0' }}>
+        <Row>
+          <Space direction='vertical' size={40}>
+            <KabadaIcon />
+            <Title level={3} style={{ ...textColor, marginBottom: '0px' }}>
+              Reset your password
+            </Title>
+          </Space>
+        </Row>
 
-    render() {
-        return (
-            <Card style={{ ...cardStyle, ...textColor }} bodyStyle={{ padding: "0" }}>
-                <Row>
-                    <Space direction="vertical" size={40}>
-                        <KabadaIcon />
-                        <Title level={3} style={{ ...textColor, marginBottom: '0px' }}>Reset your password</Title>
-                    </Space>
-                </Row>
+        <Row>
+          <Space style={{ marginBottom: '32px', paddingTop: '16px' }}>
+            Enter the email address associated with your account and we'll send
+            you a link to reset your password.
+          </Space>
+        </Row>
 
-                <Row>
-                    <Space style={{ 'marginBottom': '32px', paddingTop: '16px' }}>
-                        Enter the email address associated with your account and we'll send you a link to reset your password.
-                    </Space>
-                </Row>
+        <Form layout='vertical' name='basic' onFinish={(e) => this.onFinish(e)}>
+          <Form.Item
+            name={'email'}
+            label='Email'
+            rules={[{ required: true, type: 'email', message: 'Please enter your email address' }]}
+          >
+            <Input size='large'/>
+          </Form.Item>
 
-                <Form
-                    layout="vertical"
-                    name="basic"
-                    onFinish={this.onFinish} >
+          <Button
+            type='primary'
+            htmlType='submit'
+            size='large'
+            style={buttonStyle}
+            block={true}
+          >
+            Continue
+          </Button>
+        </Form>
 
-                    <Form.Item label={<label style={textColor}>Email address</label>}>
-                        <Form.Item
-                            name="email"
-                            rules={[{ type: 'email', required: true, message: 'Please enter your email address' }]}
-                        >
-                            <Input size="large" />
-                        </Form.Item>
-                    </Form.Item>
-                    <Form.Item >
-                        <Button type="primary" htmlType="submit" size='large' style={buttonStyle} block={true} onClick={this.onFinish.bind(this)}>
-                            Continue
-                        </Button>
-                    </Form.Item>
-                </Form>
-
-                <div style={spaceAlignContainer}>
-                    <Space direction="horizontal" align='center' size='large'>
-                        <Link style={linkStyle} to='/login'>Return to sign in</Link>
-                    </Space>
-                </div>
-            </Card>
-        )
-    }
+        <div style={spaceAlignContainer}>
+          <Space direction='horizontal' align='center' size='large'>
+            <Link style={linkStyle} to='/login'>
+              Return to sign in
+            </Link>
+          </Space>
+        </div>
+      </Card>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        loading: state.loading,
-        error: state.error,
-        message: state.message,
-    };
-}
+  return {
+    loading: state.loading,
+    error: state.error,
+    message: state.message,
+  };
+};
 
-export default connect(mapStateToProps, { forgotPassword })(withRouter(ResetPassword));
+export default connect(mapStateToProps, { forgotPassword })(
+  withRouter(ResetPassword)
+);
